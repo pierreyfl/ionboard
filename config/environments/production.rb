766ca178de46eb7187cd7c3dc785d02e2d727111
Pierre Yam <pierreyam@icloud.com>
2017-11-08 14:28:07 +0800

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,18 @@ Prelaunchr::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'referral.ionboardtech.com' }
   
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
+  
+  
+  
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
